06ccffc4b3f78ed9962ace79656cf29df79324e6
Princelion33 <88678459+Princelion33@users.noreply.github.com>
2023-02-03 19:19:47 +0100

    Update Brookhaven hub.lua

--- Brookhaven hub.lua
+++ Brookhaven hub.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Princelion33/Brookhaven-hub/main/Script'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Princelion33/Script-obu/main/Brookhaven%20hub.lua'))()
