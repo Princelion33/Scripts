09e9b2f92c9e1a217516ecbfc0e1bd877ba2b20f
Princelion33 <88678459+Princelion33@users.noreply.github.com>
2023-02-03 19:55:14 +0100

    Update Coolio gangster paradis roblox.lua

--- Coolio gangster paradis roblox.lua
+++ Coolio gangster paradis roblox.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Princelion33/Script-obu/main/Gangsta's%20Paradise.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Princelion33/Script-obu/main/Gangsta%20s%20Paradise.lua'))()
