9e93c59010a724fb7e549501ca7d4542a7820953
Princelion33 <88678459+Princelion33@users.noreply.github.com>
2023-02-03 19:22:24 +0100

    Update music chat hub.lua

--- music chat hub.lua
+++ music chat hub.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Princelion33/Music-chat-hub/main/music%20chat%20hub'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Princelion33/Script-obu/main/Music%20chat%20hub.lua'))()
