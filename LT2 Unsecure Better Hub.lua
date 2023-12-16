16523237e2d5275ff0964212fb00531931e37c94
Princelion33 <88678459+Princelion33@users.noreply.github.com>
2023-12-16 20:23:12 +0100

    Update LT2 Unsecure Better Hub.lua

--- LT2 Unsecure Better Hub.lua
+++ LT2 Unsecure Better Hub.lua
@@ -1,1 +1,11 @@
-
+if game.PlaceId == 13822889 then -- 🌳 Lumber Tycoon 2
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Butterisgood/Butter/main/lt2'))("")
+if game.PlaceId == 13822889 then -- 🌳 Lumber Tycoon 2
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Butterisgood/Butter/main/lt2'))("")
+elseif game.PlaceId == 6897226634 then -- Timber! 🌲
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Butterisgood/Butter/main/timber-obfuscated.lua'))("")
+elseif game.PlaceId == 537413528 then -- Build A Boat For Treasure
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Butterisgood/Butter/main/btbft.txt'))("")
+elseif game.PlaceId == 10514822281 then -- Case Opening Simulator 2
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Butterisgood/Butter/main/Case%20Opening%20Simulator%202-obfuscated.lua'))("")
+end
