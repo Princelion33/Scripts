55f559d2fe6569ba9b6d417a8151a806205e0b1c
Princelion33 <88678459+Princelion33@users.noreply.github.com>
2023-12-16 18:53:38 +0100

    Update LT2 Verification.lua

--- LT2 Verification.lua
+++ LT2 Verification.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 10042772221 then--Put the id in "idhere"
-   loadstring(game:HttpGet('https://raw.githubusercontent.com/stepforintructions/xc/main/bdfbd'))()
+   loadstring(game:HttpGet('https://raw.githubusercontent.com/Princelion33/Scripts/main/LT2%20Items%20Spawn.lua'))()
    else if game.PlaceId == 13822889 then
    game.Players.LocalPlayer:kick("This is Only For Lumber Unsecure")
    end
