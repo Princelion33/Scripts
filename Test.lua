c780b540189645091eb647524ff82df2aaca8a1f
Princelion33 <88678459+Princelion33@users.noreply.github.com>
2023-12-17 08:56:27 +0100

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -1,1 +1,6 @@
-
+if game.PlaceId == 10042772221 then--Put the id in "idhere"
+   loadstring(game:HttpGet('https://raw.githubusercontent.com/Butterisgood/Butter/main/lt2'))()
+   else if game.PlaceId == 13822889 then
+   game.Players.LocalPlayer:kick("This is Only For Lumber Unsecure")
+   end
+end
